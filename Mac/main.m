691e465ca51c2b80161be22f1bf6733dbce5ee8e
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-05 23:07:10 -0600

    Update main.m
    Delete end

--- Mac/main.m
+++ Mac/main.m
@@ -1,62 +1,81 @@
-/*
- Copyright (c) 2012-2019, Pierre-Olivier Latour
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- * The name of Pierre-Olivier Latour may not be used to endorse
- or promote products derived from this software without specific
- prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL PIERRE-OLIVIER LATOUR BE LIABLE FOR ANY
- DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+void
+end
+delete
+termination 
+end
+stop 
 
-#import <libgen.h>
 
-#import "GCDWebServer.h"
 
-#import "GCDWebServerDataRequest.h"
-#import "GCDWebServerURLEncodedFormRequest.h"
-#import "GCDWebServerMultiPartFormRequest.h"
 
-#import "GCDWebServerDataResponse.h"
-#import "GCDWebServerStreamedResponse.h"
 
-#import "GCDWebDAVServer.h"
 
-#import "GCDWebUploader.h"
 
-#ifndef __GCDWEBSERVER_ENABLE_TESTING__
-#error __GCDWEBSERVER_ENABLE_TESTING__ must be defined
-#endif
 
-typedef enum {
-  kMode_WebServer = 0,
-  kMode_HTMLPage,
-  kMode_HTMLForm,
-  kMode_HTMLFileUpload,
-  kMode_WebDAV,
-  kMode_WebUploader,
-  kMode_StreamingResponse,
-  kMode_AsyncResponse
-} Mode;
 
-@interface Delegate : NSObject <GCDWebServerDelegate, GCDWebDAVServerDelegate, GCDWebUploaderDelegate>
-@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @implementation Delegate
 
@@ -233,36 +252,7 @@ int main(int argc, const char* argv[]) {
                           requestClass:[GCDWebServerRequest class]
                           processBlock:^GCDWebServerResponse*(GCDWebServerRequest* request) {
                             NSString* html = @" \
-            <html><body> \
-              <form name=\"input\" action=\"/\" method=\"post\" enctype=\"application/x-www-form-urlencoded\"> \
-              Value: <input type=\"text\" name=\"value\"> \
-              <input type=\"submit\" value=\"Submit\"> \
-              </form> \
-            </body></html> \
-          ";
-                            return [GCDWebServerDataResponse responseWithHTML:html];
-                          }];
-        [webServer addHandlerForMethod:@"POST"
-                                  path:@"/"
-                          requestClass:[GCDWebServerURLEncodedFormRequest class]
-                          processBlock:^GCDWebServerResponse*(GCDWebServerRequest* request) {
-                            NSString* value = [[(GCDWebServerURLEncodedFormRequest*)request arguments] objectForKey:@"value"];
-                            NSString* html = [NSString stringWithFormat:@"<html><body><p>%@</p></body></html>", value];
-                            return [GCDWebServerDataResponse responseWithHTML:html];
-                          }];
-        break;
-      }
-
-      // Implements HTML file upload
-      case kMode_HTMLFileUpload: {
-        fprintf(stdout, "Running in HTML File Upload mode\n");
-        webServer = [[GCDWebServer alloc] init];
-        NSString* formHTML = @" \
-          <form name=\"input\" action=\"/\" method=\"post\" enctype=\"multipart/form-data\"> \
-          <input type=\"hidden\" name=\"secret\" value=\"42\"> \
-          <input type=\"file\" name=\"files\" multiple><br/> \
-          <input type=\"submit\" value=\"Submit\"> \
-          </form> \
+            
         ";
         [webServer addHandlerForMethod:@"GET"
                                   path:@"/"
